c706bc9d389ed8b930d90cb63896054fb58d61b4
Hello <bunnyblue.air+github@gmail.com>
2025-02-08 10:35:56 +0800

    Update DNSMasqFormat.java

--- builder/src/main/java/io/bunnyblue/dnsmasq/util/DNSMasqFormat.java
+++ builder/src/main/java/io/bunnyblue/dnsmasq/util/DNSMasqFormat.java
@@ -4,4 +4,7 @@ public class DNSMasqFormat {
     public static String format(String domain) {
         return String.format("address=/%s/127.0.0.1", domain);
     }
+    public static String formatIpset(String domain) {
+        return String.format("ipset=/%s/ad_list", domain);
+    }
 }
